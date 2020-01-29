5bb27776868afed488e37f6f43c0f02a25924d45
Ali Yoosefi <yoosefi12@gmail.com>
2020-01-29 16:15:41 +0330

    Add auto install plugins

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 sudo docker build -t ubuntu-jdk-scala:alpha-0.1 scala-2.12.8
 sudo docker build -t myvim:alpha-0.1 myvim 
 
-sudo docker run --name myvim-alpha-1 -it myvim:alpha-0.1 bash
+#sudo docker run --name myvim-alpha-1 -it myvim:alpha-0.1 bash
+sudo docker run -it myvim:alpha-0.1 bash
